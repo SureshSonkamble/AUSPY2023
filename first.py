52cced681dbd31e6d772b5bf205e6442f337b068
suresh sonkamble <suresh02021992@gmail.com>
2023-10-12 06:06:32 +0530

    first python program day 1 11-10-2023

---
+++ first.py
@@ -0,0 +1,10 @@
+age=30
+name="suresh"
+per=81.23
+print("Welcome to python programming")
+print("My Age=:",age)
+print(type(age))
+print("My Name=:",name)
+print(type(name))
+print("My Per=:",per)
+print(type(per))
